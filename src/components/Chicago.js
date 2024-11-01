4c20763330f637a76ecd919d674dea66a9ccccea
kyleqihua <kyleqihuahuang@gmail.com>
2024-11-01 15:02:07 +0800

    add about section

--- src/components/Chicago.js
+++ src/components/Chicago.js
@@ -1,4 +1,16 @@
+import Restaurant from '../assets/images/restaurant.jpg';
 const Chicago = () => {
-	return <section>about section</section>;
+	return (
+		<section>
+			<p>
+				Little Lemon is a charming and vibrant Mediterranean bistro located in
+				the heart of Chicago. Known for its fresh and flavorful dishes, the
+				restaurant emphasizes healthy and delicious cuisine made with
+				high-quality ingredients. Guests can enjoy a warm, inviting atmosphere
+				perfect for casual dining or special occasions.
+			</p>
+			<img src={Restaurant} className="rounded-lg" />
+		</section>
+	);
 };
 export default Chicago;
